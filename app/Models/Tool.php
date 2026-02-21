a2ca28d267d3f9fa176dcca7a12df8d9d491710e
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2026-02-21 13:01:51 +0700

    feat: implement tool model

---
+++ app/Models/Tool.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tool extends Model {
+  use HasFactory;
+
+  public function projects() {
+    return $this->belongsToMany(Project::class, 'project_tools', 'tool_id', 'project_id')
+      ->wherePivotNull('deleted_at')
+      ->withPivot('id');
+  }
+}
