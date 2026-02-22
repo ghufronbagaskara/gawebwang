7d50c3922313ff996f572ec998b31afac3d8b8fe
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2026-02-23 00:28:20 +0700

    feat: implement DashboardController with wallet management and transaction functionalities

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTopUpWalletRequest;
+use App\Http\Requests\StoreWithdrawWalletRequest;
+use App\Models\WalletTransaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller {
+  public function wallet() {
+    $user = Auth::user();
+
+    $wallet_transactions = WalletTransaction::where('user_id', $user->id)
+      ->orderByDesc('id')
+      ->paginate(10);
+
+    return view('dashboard.wallet', compact('wallet_transactions'));
+  }
+
+  public function withdraw_wallet() {
+    return view('dashboard.withdraw_wallet');
+  }
+
+  public function withdraw_wallet_store(StoreWithdrawWalletRequest $request) {
+    $user = Auth::user();
+
+    if ($user->wallet->balance < 100000) {
+      return redirect()->back()->withErrors([
+        'amount' => 'Saldo minimal untuk melakukan penarikan adalah 100.000'
+      ]);
+    }
+
+    DB::transaction(function () use ($request, $user) {
+      $validated = $request->validated();
+
+      if ($request->hasFile('proof')) {
+        $proofPath = $request->file('proof')->store('proofs', 'public');
+        $validated['proof'] = $proofPath;
+      }
+
+      $validated['type'] = 'Withdraw';
+      $validated['amount'] = $user->wallet->balance;
+      $validated['is_paid'] = false;
+      $validated['user_id'] = $user->id;
+
+      $newTopupWallet = WalletTransaction::create($validated);
+
+      $user->wallet->update([
+        'balance' => 0
+      ]);
+    });
+
+    return redirect()->route('dashboard.wallet');
+  }
+
+  public function topup_wallet() {
+    return view('dashboard.topup_wallet');
+  }
+
+  public function topup_wallet_store(StoreTopUpWalletRequest $request) {
+    $user = Auth::user();
+
+    DB::transaction(function () use ($request, $user) {
+      $validated = $request->validated();
+
+      if ($request->hasFile('proof')) {
+        $proofPath = $request->file('proof')->store('proofs', 'public');
+        $validated['proof'] = $proofPath;
+      }
+
+      $validated['type'] = 'Topup';
+      $validated['is_paid'] = false;
+      $validated['user_id'] = $user->id;
+
+      $newTopupWallet = WalletTransaction::create($validated);
+    });
+
+    return redirect()->route('dashboard.wallet');
+  }
+}
